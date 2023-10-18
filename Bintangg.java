cba9b6be8b26f916683c3e5ec12d2f2c08548b64
Phiniqq724 <141791615+Phiniqq724@users.noreply.github.com>
2023-10-18 14:46:49 +0700

    Add files via upload

---
+++ Bintangg.java
@@ -0,0 +1,36 @@
+public class Bintangg {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int a = 1; a <= i; a++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        for(int i = 5; i >= 1; i--) {
+            for(int a = 1; a <= i; a++){
+                System.out.print(a+" ");
+            }
+            System.out.print("\n");
+        }
+        for(int i = 1; i <= 5; i++) {
+            for(int a = 1; a <= i; a++){
+                System.out.print(a+" ");
+            }
+            System.out.print("\n");
+        }
+        for(int i = 4; i <= 5; i++) {
+            for(int a = 1; a <= i; a++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        for(int i = 4; i >= 1; i--) {
+            for(int a = 1; a <= i; a++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        
+        
+    }
+}
